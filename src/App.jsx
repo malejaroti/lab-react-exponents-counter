6915be4f47cd7f0c8cd8f460bdcc857d589b1247
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-08-16 09:41:25 +0200

    Lab first attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import "./App.css";
 import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
-import ExponentThree from "./components/ExponentThree";
-import ExponentFour from "./components/ExponentFour";
-import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
+import { useState } from "react";
+import Exponent from "./components/Exponent";
 
+function App() {
+  const [count, setCount] = useState(0);
 
-function App () {
   return (
     <div className="App">
-      <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      <h2>
+        <em>Counter</em>
+      </h2>
+
+      <Counter count={count} setCount={setCount} />
 
       <br />
-      <h2><em>Exponents</em></h2>
+      <h2>
+        <em>Exponents</em>
+      </h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <Exponent count={count} exponent={2} />
+        <Exponent count={count} exponent={3} />
+        <Exponent count={count} exponent={4} />
+        <Exponent count={count} exponent={5} />
+        <Exponent count={count} exponent={6} />
       </div>
     </div>
   );
